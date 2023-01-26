6196edf04c54c07a46e12563633147c437403088
Obasi Kong <apexbugfinder@gmail.com>
2023-01-25 16:38:45 -0800

    changing ports

--- src/app/helpers/Constants.ts
+++ src/app/helpers/Constants.ts
@@ -1,9 +1,9 @@
 export class Constants {
   // public static authority = "https://identity.portside.cyou";
 
-  public static clientRoot = 'http://localhost:8000';
-  // public static clientRoot = 'http://sbj.elenco.store';
+  // public static clientRoot = 'http://localhost:8000';
+  public static clientRoot = 'http://sbj.elenco.store';
 
-  public static apiRoot = 'http://localhost:5000/api/';
-  // public static apiRoot = 'https://webapi.sbj.elenco.store/api/';
+  // public static apiRoot = 'http://localhost:5000/api/';
+  public static apiRoot = 'https://webapi.sbj.elenco.store/api/';
 }
